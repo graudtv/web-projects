3078bf973b8b14c59746738c4f190f6093755715
graudtv <55806873+graudtv@users.noreply.github.com>
2025-06-01 00:18:04 +0300

    add timestamp service

---
+++ 06.Timestamp-Service/server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use('/', express.static(__dirname + '/../build'));
+
+function formatTime(time) {
+  let date = null;
+  if (time === 'now') {
+    date = new Date();
+  } else if (time.match(/^\d+$/)) {
+    date = new Date(parseInt(time));
+  } else {
+    date = new Date(time);
+  }
+  if (date.toString() === 'Invalid Date')
+    return {error: 'Invalid Date'};
+  return {unix: date.getTime(), utc: date.toUTCString()};
+}
+
+app.get('/api', function (req, res) {
+  res.json(formatTime('now'));
+});
+
+app.get('/api/:time', function (req, res) {
+  res.json(formatTime(req.params.time));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
---
+++ 06.Timestamp-Service/src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+const API_URL = 'http://127.0.0.1:3000/api';
+
+const DateBox = ({date}) => {
+  return (
+    <div>
+      <p>UTC: {date?.utc ?? '?'}</p>
+      <p>UNIX: {date?.unix ?? '?'}</p>
+    </div>
+  );
+}
+
+const App = () => {
+  //const date = {unix:0, utc:"Thu, 01 Jan 1970 00:00:00 GMT"};
+  const [curDate, setCurDate] = useState({});
+  const [timerDate, setTimerDate] = useState({});
+  const [customDate, setCustomDate] = useState({});
+  const date = {};
+
+  async function fetchTime(time, callback) {
+    try {
+      const response = await fetch(API_URL + '/' + time);
+      const json = await response.json();
+      callback(json);
+    } catch (e) {
+      console.log(e);
+      callback({});
+    }
+  };
+
+  const updateTimer = () => {
+    fetchTime('now', setTimerDate);
+  };
+
+  const onCustomDateUpdate = (e) => {
+    fetchTime(e.target.value, setCustomDate);
+  };
+
+  useEffect(() => {
+    const delay = 100; // ms
+    const interval = setInterval(() => {
+      fetchTime('now', setCurDate);
+    }, delay);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <h1>Timestamp Service Demo</h1>
+      <h2>Current time</h2>
+      <DateBox date={curDate} />
+      <h2>Timer</h2>
+      <button onClick={updateTimer}>Update</button>
+      <DateBox date={timerDate} />
+      <h2>Custom time</h2>
+      <input type="date" onChange={onCustomDateUpdate}/>
+      <DateBox date={customDate} />
+      <h2>API</h2>
+      <p><a href={API_URL + '/now'}>[service]/api/now</a></p>
+      <p><a href={API_URL + '/2015-12-25'}>[service]/api/2015-12-25</a></p>
+      <p><a href={API_URL + '/1451001600000'}>[service]/api/1451001600000</a></p>
+    </>
+  );
+};
+
+export default App;
